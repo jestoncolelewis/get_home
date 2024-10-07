c046140b606fd09057130d50ded901efffa53b6c
Jeston Lewis <61254261+jestoncolelewis@users.noreply.github.com>
2024-10-07 14:45:57 -0600

    fixed node issue

--- World.cs
+++ World.cs
@@ -3,17 +3,14 @@ using System;
 
 public partial class World : Node2D
 {
-	private CollisionPolygon2D _collisionPolygon2D;
-
-	private Polygon2D _polygon;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		 _collisionPolygon2D = GetNode<CollisionPolygon2D>("CollisionPolygon2D");
-		 _polygon = GetNode<Polygon2D>("Polygon2D");
+		 CollisionPolygon2D collisionPolygon2D = GetNode<CollisionPolygon2D>("StaticBody2D/CollisionPolygon2D");
+		 Polygon2D polygon = GetNode<Polygon2D>("StaticBody2D/CollisionPolygon2D/Polygon2D");
 
-		_polygon.Polygon = _collisionPolygon2D.Polygon;
+		polygon.Polygon = collisionPolygon2D.Polygon;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
