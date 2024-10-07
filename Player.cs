2f4d26ac1ebac156bc45f3a03e9b847ecb36908f
Jeston Lewis <61254261+jestoncolelewis@users.noreply.github.com>
2024-10-07 14:22:42 -0600

    updated player to template

--- Player.cs
+++ Player.cs
@@ -1,72 +1,40 @@
 using Godot;
 using System;
 
-public partial class Player : Area2D
+public partial class Player : CharacterBody2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public const float Speed = 100.0f;
+	public const float JumpVelocity = -100.0f;
+
+	public override void _PhysicsProcess(double delta)
 	{
-		ScreenSize = GetViewportRect().Size;
-	}
+		Vector2 velocity = Velocity;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-		var velocity = Vector2.Zero;
-
-		if (Input.IsActionPressed("move_right"))
+		// Add the gravity.
+		if (!IsOnFloor())
 		{
-			velocity.X += 1;
+			velocity += GetGravity() * (float)delta;
 		}
 
-		if (Input.IsActionPressed("move_left"))
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
 		{
-			velocity.X -= 1;
+			velocity.Y = JumpVelocity;
 		}
 
-		if (Input.IsActionPressed("crouch"))
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		if (direction != Vector2.Zero)
 		{
-			velocity.Y += 1;
-		}
-
-		if (Input.IsActionPressed("jump"))
-		{
-			velocity.Y -= 1;
-		}
-		
-		var animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-
-		if (velocity.Length() > 0)
-		{
-			velocity = velocity.Normalized() * Speed;
-			animatedSprite2D.Play();
+			velocity.X = direction.X * Speed;
 		}
 		else
 		{
-			animatedSprite2D.Stop();
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 		}
-		
-		Position += velocity * (float)delta;
-		Position = new Vector2(
-			x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
-			Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
-		);
 
-		if (velocity.X != 0)
-		{
-			animatedSprite2D.Animation = "walk";
-			animatedSprite2D.FlipV = false;
-			animatedSprite2D.FlipH = velocity.X < 0;
-		}
-		else if (velocity.Y != 0)
-		{
-			animatedSprite2D.Animation = "up";
-			animatedSprite2D.FlipV = velocity.Y > 0;
-		}
+		Velocity = velocity;
+		MoveAndSlide();
 	}
-
-	[Export] 
-	public int Speed { get; set; } = 100;
-
-	public Vector2 ScreenSize;
 }
