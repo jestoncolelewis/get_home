4a43bb94e13e36d98a5b17062b2e4682afe58411
Jeston Lewis <61254261+jestoncolelewis@users.noreply.github.com>
2024-10-08 15:46:49 -0600

    added space for fall

--- World.cs
+++ World.cs
@@ -7,10 +7,13 @@ public partial class World : Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		 CollisionPolygon2D collisionPolygon2D = GetNode<CollisionPolygon2D>("StaticBody2D/CollisionPolygon2D");
-		 Polygon2D polygon = GetNode<Polygon2D>("StaticBody2D/CollisionPolygon2D/Polygon2D");
+		CollisionPolygon2D collisionPolygon2D = GetNode<CollisionPolygon2D>("StaticBody2D/CollisionPolygon2D");
+		CollisionPolygon2D collisionPolygon2D2 = GetNode<CollisionPolygon2D>("StaticBody2D/CollisionPolygon2D2");
+		Polygon2D polygon = GetNode<Polygon2D>("StaticBody2D/CollisionPolygon2D/Polygon2D");
+		Polygon2D polygon1 = GetNode<Polygon2D>("StaticBody2D/CollisionPolygon2D2/Polygon2D");
 
 		polygon.Polygon = collisionPolygon2D.Polygon;
+		polygon1.Polygon = collisionPolygon2D2.Polygon;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
