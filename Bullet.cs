256d89cf8e2ae31e406d5fb2619af03c92792a05
Jeston Lewis <61254261+jestoncolelewis@users.noreply.github.com>
2024-10-09 17:02:23 -0600

    built initial functions

--- Bullet.cs
+++ Bullet.cs
@@ -3,13 +3,23 @@ using System;
 
 public partial class Bullet : RigidBody2D
 {
+	public float BulletSpeed = 3000f;
+	public float LifeTime = 3f;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		ApplyImpulse(new Vector2(), new Vector2(BulletSpeed, 0).Rotated(Rotation));
+		_SelfDestruct();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public void _SelfDestruct()
+	{
+		GetTree().CreateTimer(LifeTime);
 	}
 }
