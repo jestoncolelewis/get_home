c9e90784c5f5d7c411014b7862e445b941a1603f
Jeston Lewis <61254261+jestoncolelewis@users.noreply.github.com>
2024-10-16 19:22:54 -0600

    added moveloop

--- Hornet.cs
+++ Hornet.cs
@@ -3,6 +3,10 @@ using System;
 
 public partial class Hornet : RigidBody2D
 {
+	[Export]
+	PathFollow2D _pathFollow2D;
+	private float _speed = 10.0f;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,6 +15,17 @@ public partial class Hornet : RigidBody2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		MoveLoop(delta);
+	}
+
+	private void MoveLoop(double delta)
+	{
+		_pathFollow2D = GetParent<PathFollow2D>();
+		_pathFollow2D.SetProgress(_pathFollow2D.GetProgress() + _speed * (float)delta);
 	}
 
 	private void OnVisibleOnScreenNotifier2DScreenExited()
