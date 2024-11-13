e7e0cf785a108bd798e469b5dceecd731e924b8b
Jeston Lewis <61254261+jestoncolelewis@users.noreply.github.com>
2024-11-12 22:15:35 -0700

    updated color

--- script/GameManager.cs
+++ script/GameManager.cs
@@ -6,7 +6,7 @@ public partial class GameManager : Node
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		RenderingServer.SetDefaultClearColor(new Color(0.10f, 0.07f, 0.13f, 1.0f));
+		RenderingServer.SetDefaultClearColor(new Color(0.0f, 0.188235f, 0.0f, 1.0f));
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
