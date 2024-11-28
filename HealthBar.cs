bc2e7b50855cfd849634595c1565ca92435ef048
Jeston Lews <61254261+jestoncolelewis@users.noreply.github.com>
2024-11-27 19:35:43 -0700

    connectted health manager signal

--- HealthBar.cs
+++ HealthBar.cs
@@ -12,6 +12,8 @@ public partial class HealthBar : Node2D
 	private Sprite2D Health_4;
 	private Sprite2D Health_5;
 	
+	private HealthMonitor _healthMonitor;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -21,11 +23,60 @@ public partial class HealthBar : Node2D
 		Health_4 = GetNode<Sprite2D>("Health_4");
 		Health_5 = GetNode<Sprite2D>("Health_5");
 		
-		AddUserSignal("OnHealthChanged");
+		_healthMonitor = GetNode<HealthMonitor>("/root/HealthMonitor");
+		_healthMonitor.OnHealthChanged += OnPlayerHealthChanged;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	private void OnPlayerHealthChanged(int currentHealth)
+	{
+		if (currentHealth == 5)
+		{
+			Health_5.Texture = HealthFull;
+		}
+		else if (currentHealth < 5)
+		{
+			Health_5.Texture = HealthEmpty;
+		}
+		
+		if (currentHealth == 4)
+		{
+			Health_4.Texture = HealthFull;
+		}
+		else if (currentHealth < 4)
+		{
+			Health_4.Texture = HealthEmpty;
+		}
+		
+		if (currentHealth == 3)
+		{
+			Health_3.Texture = HealthFull;
+		}
+		else if (currentHealth < 3)
+		{
+			Health_3.Texture = HealthEmpty;
+		}
+		
+		if (currentHealth == 2)
+		{
+			Health_2.Texture = HealthFull;
+		}
+		else if (currentHealth < 2)
+		{
+			Health_2.Texture = HealthEmpty;
+		}
+
+		if (currentHealth == 1)
+		{
+			Health_1.Texture = HealthFull;
+		}
+		else if (currentHealth < 1)
+		{
+			Health_1.Texture = HealthEmpty;
+		}
 	}
 }
