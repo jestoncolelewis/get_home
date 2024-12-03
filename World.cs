1e78fc2621cb57c823770a13da77d70c0a290719
Jeston Lewis <61254261+jestoncolelewis@users.noreply.github.com>
2024-12-02 17:32:28 -0700

    added starting sequence

--- World.cs
+++ World.cs
@@ -3,13 +3,19 @@ using System;
 
 public partial class World : Node
 {
-	// Called when the node enters the scene tree for the first time.
+	Player player;
+	GameScreen gameScreen;
+	
+	private int _score;
+	
 	public override void _Ready()
 	{
-		NewGame();
+		player = GetNode<Player>("Player");
+		player.Hide();
+		gameScreen = GetNode<GameScreen>("GameScreen");
+		gameScreen.StartScreen();
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	
 	public override void _Process(double delta)
 	{
 	}
@@ -20,5 +26,9 @@ public partial class World : Node
 
 	public void NewGame()
 	{
+		player.Show();
+		_score = 0;
+		
+		gameScreen.UpdateScore(_score);
 	}
 }
