e0ac07a84dd72dc634bfb1a0eb9e9eaf09fe2d52
Jeston Lewis <61254261+jestoncolelewis@users.noreply.github.com>
2024-12-02 18:04:29 -0700

    added timer to gamescreen

--- ui/GameScreen.cs
+++ ui/GameScreen.cs
@@ -6,14 +6,20 @@ public partial class GameScreen : CanvasLayer
 	[Signal]
 	public delegate void StartGameEventHandler();
 	
+	Label timeLeft;
+	Timer timer;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		timeLeft = GetNode<Label>("TimeLeft");
+		timer = GetNode<Timer>("Timer");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		timeLeft.Text = timer.TimeLeft.ToString("0.0");
 	}
 
 	public void StartScreen()
@@ -24,6 +30,7 @@ public partial class GameScreen : CanvasLayer
 
 	private void OnStartButtonPressed()
 	{
+		GetNode<Timer>("Timer").Start();
 		GetNode<Button>("StartButton").Hide();
 		GetNode<Label>("ScoreLabel").Show();
 		GetNode<Node2D>("HealthBar").Show();
